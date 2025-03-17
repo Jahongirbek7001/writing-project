33ae78500d5f95b68ffefa17f3951c34cc26d443
Jahongirxon <xannazarovjahongir@gmail.com>
2025-03-17 06:31:55 +0500

    Ready project 17.03.2025

---
+++ constants/essayStructure.ts
@@ -0,0 +1,46 @@
+export const essayStructure = [
+    {
+        id: 1,
+        name: 'Problem & Solution',
+        structure: `
+            1.Introduction
+                -Topic paraphrase
+                -Author's point
+            2.Body-1(Problems)
+                -Topic sentence
+                -Problem-1 -> Reason/Example/Detail
+                -Problem-2 -> Reason/Example/Detail
+                -Problem-3 (optional) -> Reason/Example/Detail
+            2.Body-2(Solutions)
+                -Topic sentence
+                -Solution-1 -> Reason/Example/Detail
+                -Solution-2 -> Reason/Example/Detail
+                -Solution-3 (optional) -> Reason/Example/Detail
+            4.Conclusion
+                -In concusion/ To conclude, Topic paraphrase
+                -Author's point
+        `
+    },
+    {
+        id: 2,
+        name: 'Advantage & Disadvantage',
+        structure: `
+            1.Introduction
+                -Topic paraphrase
+                -Author's point
+            2.Body-1(Opposite side)
+                -Topic sentence
+                -Argument-1 -> Reason/Example/Detail
+                -Argument-2 -> Reason/Example/Detail
+                -Argument-3 (optional) -> Reason/Example/Detail
+            2.Body-2(Author's side)
+                -Topic sentence
+                -Argument-1 -> Reason/Example/Detail
+                -Argument-2 -> Reason/Example/Detail
+                -Argument-3 (optional) -> Reason/Example/Detail
+            4.Conclusion
+                -In concusion/ To conclude, Topic paraphrase
+                -Author's point
+        `
+    }
+]
